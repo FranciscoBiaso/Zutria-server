26677c84984f8c56d20ef95a4dd1f6e2ea93a779
FranciscoBiaso <kikinhobiaso@gmail.com>
2015-11-19 15:13:34 -0200

    player attributes table added

--- font/const.h
+++ font/const.h
@@ -347,6 +347,34 @@ enum PlayerFlags{
 	PlayerFlag_LastFlag
 };
 
+// player attributes gain per level
+// health points, physical attack, physical defense, capacity
+// mana points, magical attack, magical defense, magic points,
+// player speed, attack speed, cooldown, avoidance
+int playerAttributes[][12] = 
+{ 
+	{ 2, 1, 1, 2, 7, 5, 4, 2, 2, 1, 2, 1 } , // fire mage
+	{ 2, 1, 1, 2, 7, 5, 3, 2, 3, 1, 2, 1 } , // eletrician mage
+	{ 3, 4, 3, 3, 3, 2, 1, 1, 3, 2, 1, 4 } , // archer
+	{ 5, 5, 4, 6, 2, 1, 1, 1, 1, 1, 1, 2 } , // knight
+	{ 2, 1, 1, 2, 7, 4, 5, 2, 2, 1, 2, 1 } , // druid
+	{ 2, 1, 1, 2, 6, 4, 5, 2, 2, 1, 3, 1 } , // sumoner
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Reserved player storage key ranges
 //[10000000 - 20000000]
 #define PSTRG_RESERVED_RANGE_START  10000000
